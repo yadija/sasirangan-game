d4cce62dbf2477dca3467fa15befd2f5ac113201
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-12-12 14:50:57 +0800

    ✨feat: install and setup react router

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,18 @@
+import { useRoutes } from "react-router";
+
 export default function App() {
-  return <h1 className="text-3xl">Hello World</h1>;
+  const element = useRoutes([
+    {
+      path: "/",
+      element: <div>Home</div>,
+    },
+  ]);
+
+  return (
+    <main className="m-auto grid h-screen bg-[url('https://i.pinimg.com/736x/d5/4a/1e/d54a1e5fd3b883bb410ae11a64f46c08.jpg')]">
+      <section className="m-auto h-[90%] w-4/5 rounded bg-white/30 backdrop-blur">
+        {element}
+      </section>
+    </main>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,14 @@ import "./index.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
 
 import App from "./app.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
