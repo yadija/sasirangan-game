37e9cd28aae9e900eaca842f79312b1f67c0ed28
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-12-16 10:10:31 +0800

    ✨feat: add gretting and change glass background style

--- src/app.tsx
+++ src/app.tsx
@@ -23,7 +23,15 @@ export default function App() {
 
   return (
     <main className="m-auto grid h-screen bg-cover bg-[url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd98YXF0irQENhSBfItW4RYoPSdyxMMX7YXQ&s')]">
-      <section className="m-auto h-[90%] w-4/5 rounded bg-white/30 backdrop-blur">
+      <section className="m-auto h-[90%] w-4/5 rounded bg-white/50 backdrop-blur-lg">
+        <nav className="absolute p-2">
+          <h1>
+            {localStorage.getItem("name")
+              ? `Halo, ${localStorage.getItem("name")}`
+              : "Selamat Datang"}
+          </h1>
+        </nav>
+
         {element}
       </section>
     </main>
