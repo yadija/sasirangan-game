230e20e3a0d9494f688a621c3af35eeff5166687
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-12-16 08:49:58 +0800

    ✨feat: implement local storage to keep name

--- src/pages/home/section/start.button.tsx
+++ src/pages/home/section/start.button.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router";
 
 import {
@@ -12,9 +13,61 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 
 export default function StartButton() {
+  const [name, setName] = useState("");
+  const [open, setOpen] = useState(false);
+
   const navigate = useNavigate();
+
+  const onSubmit = () => {
+    localStorage.setItem("name", name);
+    setOpen(false);
+    navigate(0);
+  };
+
+  if (!localStorage.getItem("name"))
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button>Mulai Quiz</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Tambahkan Nama</DialogTitle>
+            <DialogDescription>
+              Silahkan masukkan nama anda untuk mengikuti quiz
+            </DialogDescription>
+          </DialogHeader>
+
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Input
+              id="name"
+              value={name}
+              placeholder="Masukkan Nama"
+              onChange={(event) => setName(event.target.value)}
+              className="col-span-3"
+            />
+          </div>
+
+          <DialogFooter>
+            <Button type="submit" onClick={onSubmit}>
+              Simpan
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
 
   return (
     <AlertDialog>
