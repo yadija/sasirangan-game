1e12ca630629391319d258f91719acde4a5de0aa
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-12-18 17:20:13 +0800

    ✨feat: make and implement theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
-import App from "./app.tsx";
+import App from "@/app.tsx";
+import { ThemeProvider } from "@/providers/theme-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <ThemeProvider storageKey="sasirangan-game-ui-theme">
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  </ThemeProvider>,
 );
