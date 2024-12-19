0449dffe9aa92394594ccef3f5529282855ec9df
Achmad Yuliadi <ayuliadi01@gmail.com>
2024-12-19 09:47:19 +0800

    ✨feat: add condition dialog in navbar

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -1,6 +1,6 @@
 import { Pencil } from "lucide-react";
 import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
 
 import { ModeToggle } from "@/components/common/mode-toggle";
 import { Button } from "@/components/ui/button";
@@ -20,6 +20,7 @@ export default function Navbar() {
   const [open, setOpen] = useState(false);
 
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   const onSubmit = () => {
     localStorage.setItem("name", name);
@@ -30,39 +31,43 @@ export default function Navbar() {
   return (
     <nav className="absolute flex w-full items-center justify-between gap-2 p-2">
       <section className="flex items-center gap-2">
-        <Dialog open={open} onOpenChange={setOpen}>
-          <DialogTrigger asChild>
-            <Button variant="ghost">
-              <Pencil />
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>
-                {localStorage.getItem("name") ? "Ubah Nama" : "Tambahkan Nama"}
-              </DialogTitle>
-              <DialogDescription>
-                Silahkan masukkan nama anda untuk mengikuti quiz
-              </DialogDescription>
-            </DialogHeader>
-
-            <div className="grid grid-cols-4 items-center gap-4">
-              <Input
-                id="name"
-                value={name}
-                placeholder="Masukkan Nama"
-                onChange={(event) => setName(event.target.value)}
-                className="col-span-3"
-              />
-            </div>
-
-            <DialogFooter>
-              <Button type="submit" onClick={onSubmit}>
-                Simpan
+        {pathname === "/" && (
+          <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+              <Button variant="ghost">
+                <Pencil />
               </Button>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>
+                  {localStorage.getItem("name")
+                    ? "Ubah Nama"
+                    : "Tambahkan Nama"}
+                </DialogTitle>
+                <DialogDescription>
+                  Silahkan masukkan nama anda untuk mengikuti quiz
+                </DialogDescription>
+              </DialogHeader>
+
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Input
+                  id="name"
+                  value={name}
+                  placeholder="Masukkan Nama"
+                  onChange={(event) => setName(event.target.value)}
+                  className="col-span-3"
+                />
+              </div>
+
+              <DialogFooter>
+                <Button type="submit" onClick={onSubmit}>
+                  Simpan
+                </Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        )}
 
         <h1 className="text-sm font-bold sm:text-base">
           {localStorage.getItem("name")
